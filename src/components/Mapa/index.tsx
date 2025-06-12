36cffce4362ac54829e7f945eb176cc1a163b136
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-06-12 10:13:22 -0300

    acionando loading lazy nos mapas

--- src/components/Mapa/index.tsx
+++ src/components/Mapa/index.tsx
@@ -11,6 +11,7 @@ function Mapa() {
         perfeitas para quem busca aventura e contato com a natureza.
       </p>
       <iframe
+        loading="lazy"
         title="Google maps"
         className="w-full h-[30rem] lg:h-[35rem]"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.3469548877215!2d-45.66375792387372!3d-22.67812457941675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc7de7a1085cab%3A0xfbaf5cb9454d9009!2sRef%C3%BAgio%20da%20Pedra%20Sp!5e0!3m2!1spt-BR!2sbr!4v1741976848192!5m2!1spt-BR!2sbr"
